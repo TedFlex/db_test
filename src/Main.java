d271744cec8f0cfe850fa5831fc1bdb83a35989f
TedFlex <munsifalikhan62@gmail.com>
2023-01-28 17:06:41 +0500

    ComboBox for GunType

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.Arrays;
+
 public class Main extends JFrame implements ActionListener {
 static JComboBox Side;
 static JComboBox gender;
-static JButton jb;
+static JComboBox GunType;
 static JButton filter;
 static Connection conn;
 static Statement st;
@@ -17,11 +19,8 @@ static ResultSet rs;
              conn=DriverManager.getConnection("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true;","Ted2","m16isopaf");
              st=conn.createStatement();
              rs=st.executeQuery("SELECT OPS.*,GUNS.name AS GunName FROM OPS JOIN GUNS ON OPS.gunid=GUNS.id");
-            while (rs.next())
-            {
-                System.out.print(rs.getString(1)+" "+rs.getString("GunName"));
-                System.out.println();
-            }
+
+
         }catch (Exception e){
             System.out.println(e);
         }
@@ -38,22 +37,35 @@ static ResultSet rs;
         Side.addItem("DEFENDER");
         Side.addItem("ALL");
         Side.setSelectedItem("ALL");
+        GunType=new JComboBox();
+        GunType.addItem("AR");
+        GunType.addItem("DMR");
+        GunType.addItem("SLUG");
+        GunType.addItem("SMG");
+        GunType.addItem("LMG");
+        GunType.addItem("ALL");
+        GunType.setSelectedItem("ALL");
 
         main.add(gender);
+        main.add(GunType);
         gender.setBounds(250,50,100,30);
+        GunType.setBounds(400,50,100,30);
         gender.addItem("FEMALE");
-        gender.addItem("ATTACKER");
-        gender.addItem("BOTH");
+        gender.addItem("MALE");
+        gender.addItem("ALL");
         gender.setSelectedItem("ALL");
         JLabel sidelabel=new JLabel("Side");
         JLabel genderlabel=new JLabel("Gender");
+        JLabel guntypelabel=new JLabel("Guntype");
         main.add(sidelabel);
         main.add(genderlabel);
         sidelabel.setBounds(100,20,70,30);
         genderlabel.setBounds(250,20,70,30);
+        guntypelabel.setBounds(400,20,70,30);
         filter=new JButton("Filter");
         filter.addActionListener(main);
         main.add(filter);
+        main.add(guntypelabel);
         filter.setBounds(100,100,70,30);
 
         main.setVisible(true);
@@ -64,7 +76,7 @@ static ResultSet rs;
         if (e.getSource() == filter) {
             if (gender.getSelectedItem().equals("FEMALE") && Side.getSelectedItem().equals("ATTACKER")) {
                 try {
-                rs=st.executeQuery("SELECT OPS.*, GUNS.name,GUNS.type,GUNS.ammo,GUNS.rating FROM OPS JOIN GUNS ON OPS.gunid=GUNS.id WHERE OPS.side='ATTACKER' AND OPS.gender='FEMALE'");
+                rs=st.executeQuery("SELECT OPS.*, GUNS.name AS GunName,GUNS.type,GUNS.ammo,GUNS.rating FROM OPS JOIN GUNS ON OPS.gunid=GUNS.id WHERE OPS.side='ATTACKER' AND OPS.gender='FEMALE'");
                 while (rs.next()){
                     System.out.print(rs.getString("name"));
                     System.out.println();
@@ -77,7 +89,37 @@ static ResultSet rs;
                 try {
                     rs=st.executeQuery("SELECT OPS.*, GUNS.name,GUNS.type,GUNS.ammo,GUNS.rating FROM OPS JOIN GUNS ON OPS.gunid=GUNS.id WHERE OPS.side='DEFENDER' AND OPS.gender='FEMALE'");
                     while (rs.next()){
+                        System.out.print(rs.getString("name")+" "+rs.getString("tier"));
+                        System.out.println();
+                    }
+                } catch (Exception a) {
+                    System.out.println(a);
+                }
+            }else if (gender.getSelectedItem().equals("ALL")  && Side.getSelectedItem().equals("ALL")){
+                try {
+                    rs=st.executeQuery("SELECT OPS.*, GUNS.name,GUNS.type,GUNS.ammo,GUNS.rating FROM OPS JOIN GUNS ON OPS.gunid=GUNS.id");
+                    while (rs.next()){
                         System.out.print(rs.getString("name"));
+                        System.out.println();
+                    }
+                } catch (Exception a) {
+                    System.out.println(a);
+                }
+            }else if (gender.getSelectedItem().equals("MALE")  && Side.getSelectedItem().equals("DEFENDER")){
+                try {
+                    rs=st.executeQuery("SELECT OPS.*, GUNS.name,GUNS.type,GUNS.ammo,GUNS.rating FROM OPS JOIN GUNS ON OPS.gunid=GUNS.id WHERE OPS.side='DEFENDER' AND OPS.gender='MALE'");
+                    while (rs.next()){
+                        System.out.print(rs.getString("name")+" "+rs.getString("tier"));
+                        System.out.println();
+                    }
+                } catch (Exception a) {
+                    System.out.println(a);
+                }
+            }else if (gender.getSelectedItem().equals("MALE")  && Side.getSelectedItem().equals("ATTACKER")){
+                try {
+                    rs=st.executeQuery("SELECT OPS.*, GUNS.name,GUNS.type,GUNS.ammo,GUNS.rating FROM OPS JOIN GUNS ON OPS.gunid=GUNS.id WHERE OPS.side='ATTACKER' AND OPS.gender='MALE'");
+                    while (rs.next()){
+                        System.out.print(rs.getString("name")+" "+rs.getString("tier"));
                         System.out.println();
                     }
                 } catch (Exception a) {
